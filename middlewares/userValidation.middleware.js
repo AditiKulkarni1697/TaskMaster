722f9bf74d0810734ff6dc8d1ab9f91657acde87
AditiSujlegaonkar1697 <aditisujlegaonkar@gmail.com>
2025-01-30 22:01:31 +0530

    user validation updated

--- middlewares/userValidation.middleware.js
+++ middlewares/userValidation.middleware.js
@@ -5,6 +5,19 @@ const userValidation = async (req,res,next) => {
         return res.status(400).send({msg:"Missing required fields"});
     }
 
+    if(name.length < 3 || name.length > 50){
+        return res.status(400).send({msg:"Name must be between 3 to 50 characters"});
+    }
+
+    const emailRegex = "^[^\s@]+@[^\s@]+\.[^\s@]+$";
+    if(!emailRegex.test(email)){
+        return res.status(400).send({msg:"Invalid email"});
+    }
+
+    if(password.length < 8 || password.length > 50){
+        return res.status(400).send({msg:"Password must be between 8 to 50 characters"});
+    }
+
     next();
 }
 
